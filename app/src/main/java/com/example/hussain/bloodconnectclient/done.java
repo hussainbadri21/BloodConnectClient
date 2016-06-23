c88b22dbf3b4d8dd742c98eabd5863b4e76ec578
Hussain <hussain_bri2@yahoo.com>
2016-06-24 01:29:42 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/hussain/bloodconnectclient/done.java
@@ -0,0 +1,14 @@
+package com.example.hussain.bloodconnectclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class done extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.done);
+    }
+}
